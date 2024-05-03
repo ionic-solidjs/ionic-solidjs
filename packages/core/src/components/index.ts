de88570fe787af135ab62843bbec1f9207bac3b9
Hookyns <hookcze@gmail.com>
2024-05-03 14:08:40 +0200

    Add the Ionic JSX namespace to the solid-js' JSX namespace so developers can use <ion-xx> web components directly

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,10 +1,15 @@
+import type {JSX as IonicJSX} from '@ionic/core/dist/types/components';
+import type { JSXBase } from '@ionic/core/dist/types/stencil-public-runtime';
+import type { ParentProps } from 'solid-js';
+
+type WithElementsAndChildren<T> = {
+	[P in keyof T]?: ParentProps<T[P] & (P extends keyof HTMLElementTagNameMap ? JSXBase.HTMLAttributes<HTMLElementTagNameMap[P]> : {})>;
+};
+
+// Add the Ionic JSX namespace to the solid-js' JSX namespace
 declare module 'solid-js' {
 	export namespace JSX {
-		interface IntrinsicElements {
-			'ion-router': any;
-			'ion-route': any;
-			'ion-router-outlet': any;
-			'ion-router-link': any;
+		interface IntrinsicElements extends WithElementsAndChildren<IonicJSX.IntrinsicElements> {
 		}
 	}
 }
