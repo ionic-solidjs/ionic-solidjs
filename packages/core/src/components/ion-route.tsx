c57389f6b92e4439c03c93c6e1b7498882c98239
Hookyns <hookcze@gmail.com>
2024-09-04 02:24:54 +0200

    fix: Fix of Contexts and Cleanups of pages when using Router

--- packages/core/src/components/ion-route.tsx
+++ packages/core/src/components/ion-route.tsx
@@ -1,37 +1,10 @@
 import type { JSX as IonicJSX } from '@ionic/core';
 import { defineCustomElement } from '@ionic/core/components/ion-route';
-import { type Component, type JSX as JSXBase, splitProps } from 'solid-js';
-import { RouterContextProvider, useRouter } from '../hooks';
+import { getOwner, type JSX as JSXBase, splitProps } from 'solid-js';
 import type { FixIonProps } from '../lib';
-import {
-	ComponentRefElementName,
-	type ComponentRefProps,
-	type RefComponentProps,
-} from './ComponentRef';
+import { ComponentRefElementName } from './ComponentRef';
 
 defineCustomElement();
-
-type RouteRouterContextProviderProps = ComponentRefProps<Component> & {
-	router: HTMLIonRouterElement | undefined;
-};
-
-// Wrapper around the route component providing correct value for RouterContext
-function RouteRouterContextProvider(props: RefComponentProps<RouteRouterContextProviderProps>) {
-	/*
-	 * A shadow root prevents ion-modal from finding its trigger elements.
-	 * `noShadowDOM()` - is not working; it fails because it's not able to find the element.
-	 * TODO: Why is this required here? Is is required? Should we force no shadow DOM? Shouldn't it be configurable?
-	 */
-	Object.defineProperty(props.componentOptions.element, 'renderRoot', {
-		value: props.componentOptions.element,
-	});
-
-	return (
-		<RouterContextProvider value={props.router}>
-			{props.component(props.componentProps)}
-		</RouterContextProvider>
-	);
-}
 
 export type IonRouteProps<TComponent> = FixIonProps<
 	Omit<IonicJSX.IonRoute, 'component' | 'componentProps'>
@@ -51,7 +24,6 @@ export type IonRouteProps<TComponent> = FixIonProps<
 export function IonRoute<TComponent extends string | ((...args: any[]) => any) = string>(
 	props: IonRouteProps<TComponent>
 ) {
-	const router = useRouter();
 	const [_, restProps] = splitProps(props, [
 		'component',
 		'componentProps',
@@ -70,12 +42,9 @@ export function IonRoute<TComponent extends string | ((...args: any[]) => any) =
 		typeof props.component === 'string'
 			? undefined
 			: {
-					component: RouteRouterContextProvider,
-					componentProps: {
-						component: props.component,
-						componentProps: props.componentProps ?? {},
-						router: router,
-					} satisfies RouteRouterContextProviderProps,
+					component: props.component,
+					componentProps: props.componentProps ?? {},
+					owner: getOwner(),
 				};
 
 	return (
