09d764d6495ad10c1e690664fa0eb891269c0581
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2025-07-19 11:58:09 +0100

    fix: add test placeholders

---
+++ packages/core/todo.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'bun:test';
+
+describe('TODO', () => {
+  it('should add real tests', () => {
+    // TODO: Add actual tests for @ionic-solidjs/core
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/ionicons/todo.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'bun:test';
+
+describe('TODO', () => {
+  it('should add real tests', () => {
+    // TODO: Add actual tests for @ionic-solidjs/ionicons
+    expect(true).toBe(true);
+  });
+});
